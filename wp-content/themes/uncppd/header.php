d4440435d406a246fd1a8780c5111133985c48a9
Benjamin Bertout <bbertout@beapi.fr>
2018-04-02 20:34:31 +0200

    update logo and fonts

--- wp-content/themes/uncppd/header.php
+++ wp-content/themes/uncppd/header.php
@@ -14,32 +14,32 @@
     <title>Uncppd</title>
 
     <!-- Web App favicons from /assets/img/favicons/index_hd.html -->
-        <link rel="manifest" href="src/assets/img/favicons/manifest.json">
+        <link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/manifest.json">
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="theme-color" content="#ffffff">
         <meta name="application-name" content="BFF">
-        <link rel="apple-touch-icon" sizes="57x57" href="src/assets/img/favicons/apple-touch-icon-57x57.png">
-        <link rel="apple-touch-icon" sizes="60x60" href="src/assets/img/favicons/apple-touch-icon-60x60.png">
-        <link rel="apple-touch-icon" sizes="72x72" href="src/assets/img/favicons/apple-touch-icon-72x72.png">
-        <link rel="apple-touch-icon" sizes="76x76" href="src/assets/img/favicons/apple-touch-icon-76x76.png">
-        <link rel="apple-touch-icon" sizes="114x114" href="src/assets/img/favicons/apple-touch-icon-114x114.png">
-        <link rel="apple-touch-icon" sizes="120x120" href="src/assets/img/favicons/apple-touch-icon-120x120.png">
-        <link rel="apple-touch-icon" sizes="144x144" href="src/assets/img/favicons/apple-touch-icon-144x144.png">
-        <link rel="apple-touch-icon" sizes="152x152" href="src/assets/img/favicons/apple-touch-icon-152x152.png">
-        <link rel="apple-touch-icon" sizes="180x180" href="src/assets/img/favicons/apple-touch-icon-180x180.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/apple-touch-icon-180x180.png">
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="apple-mobile-web-app-title" content="BFF">
-        <link rel="icon" type="image/png" sizes="228x228" href="src/assets/img/favicons/coast-228x228.png">
+        <link rel="icon" type="image/png" sizes="228x228" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/coast-228x228.png">
         <meta name="msapplication-TileColor" content="#ffffff">
-        <meta name="msapplication-TileImage" content="src/assets/img/favicons/mstile-144x144.png">
-        <meta name="msapplication-config" content="src/assets/img/favicons/browserconfig.xml">
-        <link rel="yandex-tableau-widget" href="src/assets/img/favicons/yandex-browser-manifest.json">
+        <meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/mstile-144x144.png">
+        <meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/browserconfig.xml">
+        <link rel="yandex-tableau-widget" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/yandex-browser-manifest.json">
 
         <!-- Standard favicons from /assets/img/favicons/index_sd.html -->
-        <link rel="icon" type="image/png" sizes="32x32" href="src/assets/img/favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="src/assets/img/favicons/favicon-16x16.png">
-        <link rel="shortcut icon" href="src/assets/img/favicons/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/favicon-16x16.png">
+        <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/img/favicons/favicon.ico">
 
     <?php wp_head();?>
 </head>
